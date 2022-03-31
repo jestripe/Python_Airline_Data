a89cb7d71cfd69ec3855e03d43ca775a549a91b2
jesse.stripe <jesse.stripe@gmail.com>
2022-03-31 15:32:58 -0500

    Working API query

--- City_pairs.py
+++ City_pairs.py
@@ -1,12 +1,27 @@
 from email.mime import base
 import requests
 import json
+import pandas as pd
+
+api_filename = 'api_key.json'
+
+api_keys = {}
+with open(api_filename, 'r') as f:
+    api_secret = json.loads(f.read())
+
+api_key = api_secret['AVIATION_STACK_SECRET']
 
 base_url = 'http://api.aviationstack.com/v1/flights'
-api_key = 'SECRET'
 
-params = dict(access_key = api_key, dep_iata = 'DFW', arr_iata = 'MSP', status = 'active', airline_icao = 'aal')
+params = dict(access_key = api_key, dep_iata = 'DFW', ar_iata = 'MSP', status = 'active', airline_icao = 'aal')
 
-response = requests.get(base_url, params)
+# response.json()
 
-print(response.text)
+# df = pd.DataFrame.from_dict(response.json()['data'])
+
+# df.to_csv('test.csv')
+
+response.json().keys()
+
+t2 = response.json()['data']
+df = pd.json_normalize(t2)
